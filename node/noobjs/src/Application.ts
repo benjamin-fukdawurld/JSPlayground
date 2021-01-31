7b3c83f04a12412d1f5b677b40d061d835ad9be2
Benjamin Zamour <benjamin.zamour@gmail.com>
2021-01-31 11:34:03 +0100

    implemented ts Application wrapper

--- node/noobjs/src/Application.ts
+++ node/noobjs/src/Application.ts
@@ -3,7 +3,7 @@ import http from 'http';
 import https from 'https';
 
 export type ServerGenerator = (app: Express.Application) => http.Server | https.Server | null;
-
+type Server = http.Server | https.Server;
 export default class Application {
     private _expressApp: express.Application;
     private _serverGenerator: ServerGenerator;
@@ -23,11 +23,13 @@ export default class Application {
         return this._server;
     }
 
-    start(hostname: string, port: number): void {
+    start(hostName: string, port: number): void {
         if (!this._server) {
-            throw new Error(
-                "Server must be created via Applicaiton.createServer before starting Application"
-            );
+            this.createServer();
+        }
+
+        if(!this._server) {
+            throw new Error("The server has not been created, something went wrong");
         }
 
         this._server.listen(port, hostName);
@@ -35,5 +37,66 @@ export default class Application {
 
     get expressApp() {
         return this._expressApp;
+    }
+
+    get(path: string, ...args : any) {
+        return this._expressApp.get(path, ...args);
+    }
+
+    post(path: string, ...args : any) {
+        return this._expressApp.post(path, ...args);
+    }
+
+    put(path: string, ...args : any) {
+        return this._expressApp.put(path, ...args);
+    }
+
+    patch(path: string, ...args : any) {
+        return this._expressApp.patch(path, ...args);
+    }
+
+    delete(path: string, ...args : any) {
+        return this._expressApp.delete(path, ...args);
+    }
+
+    head(path: string, ...args : any) {
+        return this._expressApp.head(path, ...args);
+    }
+
+    trace(path: string, ...args : any) {
+        return this._expressApp.trace(path, ...args);
+    }
+
+    connect(path: string, ...args : any) {
+        return this._expressApp.connect(path, ...args);
+    }
+
+    options(path: string, ...args : any) {
+        return this._expressApp.options(path, ...args);
+    }
+
+    addRoute(method: string, path: string, ...args: any) {
+        if(method === "get")
+            this.get(path, ...args);
+        if(method === "post")
+            this.post(path, ...args);
+        if(method === "put")
+            this.put(path, ...args);
+        if(method === "patch")
+            this.patch(path, ...args);
+        if(method === "delete")
+            this.delete(path, ...args);
+        if(method === "head")
+            this.head(path, ...args);
+        if(method === "trace")
+            this.trace(path, ...args);
+        if(method === "connect")
+            this.connect(path, ...args);
+        if(method === "options")
+            this.options(path, ...args);
+    }
+
+    addRoutes(routes: {method: string, path: string, args: any[]}[]) {
+        routes.forEach(route => this.addRoute(route.method, route.path, ...route.args));
     }
 }
--- node/noobjs/src/index.ts
+++ node/noobjs/src/index.ts
@@ -1,26 +1,22 @@
 import * as dotenv from 'dotenv';
-import express from 'express';
 import https from 'https';
 import http from 'http';
+import {Request, Response} from 'express';
 
 import Application from './Application';
 
 dotenv.config();
 const protocol = (process.env.SERVER_HTTPS === "true" ? https : http);
-const hostName = process.env.SERVER_HOST;
+const hostName = process.env.SERVER_HOST || "127.0.0.1";
 const port = parseInt(process.env.SERVER_PORT || "3000", 10);
 
 const app = new Application(protocol.createServer);
-app.expressApp.get('/', (req: any, res: any) => {
-    res.send("hello world");
-});
+app.addRoutes([
+    {
+        method: "get",
+        path: "/",
+        args: [ (req: Request, res: Response) => { res.send("hello world"); } ]
+    }
+]);
 
-const server = protocol.createServer(app);
-server.listen(port, hostName);
-
-
-process.on('SIGTERM', () => {
-    server.close(() => {
-        console.log('Server terminated')
-    })
-});
+app.start(hostName, port);
